8e114a97aa7027ee7181fc1579a53a1907527628
Egil <egil.moller@freecode.no>
2018-03-06 13:59:12 +0100

    Added setup.py

---
+++ cloudberry_docker_manager/cli.py
@@ -0,0 +1,25 @@
+import click
+import cloudberry_docker_manager.apply
+import cloudberry_openwisp
+import os.path
+
+CONFIG = os.path.expanduser("~/.config/cloudberry-docker-manager.json")
+
+@click.group()
+def main(**kw):
+    pass
+    
+@main.command()
+@click.argument("base_url")
+@click.argument("uuid")
+@click.argument("key")
+def init(base_url, uuid, key):
+    """Initialize a the docker manager"""
+    config = cloudberry_openwisp.Config(CONFIG)
+
+@main.command()
+def update():
+    """Update the currently running docker containers with the config from OpenWISP server"""
+    config = cloudberry_openwisp.Config(CONFIG)
+    config.update()
+    cloudberry_docker_manager.apply.apply(config)
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(name='cloudberry-docker-manager',
+      version='0.1',
+      description='A docker container manager for Cloudberry',
+      author='Egil Moeller',
+      author_email='egil@innovationgarage.no',
+      url='https://github.com/innovationgarage/cloudberry-docker-manager',
+      packages=setuptools.find_packages(),
+      install_requires=[
+          'cloudberry-netjson',
+          'docker',
+          'click'
+      ],
+      entry_points='''
+      [console_scripts]
+      cloudberry-docker-manager = cloudberry_docker_manager.cli:main
+      '''
+  )
